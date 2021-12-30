7d4c1581e40a4462bf5c260eebd21074a9826395
Chandni Singh <chandni1141996@gmail.com>
2021-12-30 22:26:23 +0530

    created hompage files

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
+import HomePage from './HomePage';
 import "./App.css";
 
 function App() {
-  return <div>Hello World</div>;
+  return <div><HomePage /></div>;
 }
 
 export default App;
