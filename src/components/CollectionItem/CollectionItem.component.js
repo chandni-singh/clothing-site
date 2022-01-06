de2533639429a3bf0ae934eeacc68f1d57ebf735
Chandni Singh <chandni1141996@gmail.com>
2022-01-06 15:57:25 +0530

    Implemented cart Item reducer

--- src/components/CollectionItem/CollectionItem.component.js
+++ src/components/CollectionItem/CollectionItem.component.js
@@ -1,16 +1,25 @@
-import React from 'react';
-import CustomButton from '../CustomButton/CustomButton.component';
-import './CollectionItem.styles.scss';
+import React from "react";
+import { connect } from "react-redux";
+import { addItem } from "../../Redux/cart/cart.actions";
+import CustomButton from "../CustomButton/CustomButton.component";
+import "./CollectionItem.styles.scss";
 
-const CollectionItem = ({id, name, price, imageUrl}) => (
-    <div className='collection-item' key={id}>
-        <div className='image' style={{backgroundImage: `url(${imageUrl})`}} />
-        <div className='collection-footer'>
-            <span className='name'>{name}</span>
-            <span className='price'>{price}</span>
-        </div>
-        <CustomButton inverted>Add to cart</CustomButton>
+const CollectionItem = ({item, addItem}) => {
+  const { id, name, price, imageUrl } = item;
+  return (
+    <div className="collection-item" key={id}>
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-)
+  );
+};
 
-export default CollectionItem;
+const mapDispatchToProps = (dispatch) => ({
+  addItem: (item) => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
