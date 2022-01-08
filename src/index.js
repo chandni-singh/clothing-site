edf0b17e222959368a41ed36a057858a934940e9
Chandni Singh <chandni1141996@gmail.com>
2022-01-08 19:45:26 +0530

    Added persistance to app using redux persistor library

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import { Provider } from "react-redux";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import store from '../src/Redux/store';
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "../src/Redux/store";
+import store from "../src/Redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
